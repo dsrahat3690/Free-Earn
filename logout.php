acc52f6259e81342ae0e246dce44921510a6a934
dsrahat3690 <121874637+dsrahat3690@users.noreply.github.com>
2025-07-04 18:20:39 +0600

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,1 +1,8 @@
 
+<?php
+session_start();
+session_unset();  // সব সেশন ভ্যারিয়েবল ক্লিয়ার করে
+session_destroy(); // সেশন ধ্বংস করে
+
+header("Location: login.php"); // লগইন পেইজে পাঠিয়ে দেয়
+exit();
